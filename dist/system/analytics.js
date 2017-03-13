7da312032dc3cafe88f4f7326d746a0d540f9528
Jon Carlson <jdcarlson@gmail.com>
2017-03-13 12:50:22 -0400

    Remove debug google analytics

--- dist/system/analytics.js
+++ dist/system/analytics.js
@@ -102,14 +102,13 @@ System.register(['aurelia-dependency-injection', 'aurelia-event-aggregator', 'au
 
 				Analytics.prototype.init = function init(id) {
 					var script = document.createElement('script');
-					script.text = "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){" + "(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o)," + "m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)" + "})(window,document,'script','https://www.google-analytics.com/analytics_debug.js','ga');";
+					script.text = "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){" + "(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o)," + "m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)" + "})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');";
 					document.querySelector('body').appendChild(script);
 
 					window.ga = window.ga || function () {
 						(ga.q = ga.q || []).push(arguments);
 					};
 					ga.l = +new Date();
-					window.ga_debug = { trace: true };
 					ga('create', id, 'auto');
 
 					this._initialized = true;
